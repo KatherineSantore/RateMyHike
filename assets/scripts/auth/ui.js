e02ca46c87748fae595d744d7af30cf68ca7bdbd
KatherineWard <katherine.eileen.ward@gmail.com>
2018-07-15 21:07:31 -0400

    Fixed CP UI message

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -7,7 +7,10 @@ const signUpSuccess = function (signUpResponse) {
 }
 
 const signUpError = function () {
-  $('#signUpModalLabel').html('You are not signed up! Try again')
+  $('#signUpModalLabel').html('You are not signed up! Try Again!')
+  // setTimeout(function () {
+  //   $('#signUpModalLabel').html('')
+  // }, 5000)
   document.getElementById('sign-up-form').reset()
 }
 
@@ -24,16 +27,25 @@ const signInSuccess = function (response) {
 }
 const signInError = function (response) {
   $('#signInModalLabel').html('Please try again!')
+  setTimeout(function () {
+    $('#signInModalLabel').fadeOut().empty()
+  }, 4000)
   document.getElementById('sign-in-form').reset()
 }
 
 const changePasswordSuccess = function (response) {
   $('#cpModalLabel').html('Your password is changed!')
+  setTimeout(function () {
+    $('#cpModalLabel').fadeOut().empty()
+  }, 4000)
   document.getElementById('cp-form').reset()
 }
 
 const changePasswordError = function (response) {
   $('#cpModalLabel').html('Your password is not changed! Try again.')
+  setTimeout(function () {
+    $('#cpModalLabel').fadeOut().empty()
+  }, 4000)
   document.getElementById('cp-form').reset()
 }
 
